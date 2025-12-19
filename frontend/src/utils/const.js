52eed342ba9642f15e3df9668e59c16b363aa879
khushalishah2611 <khushalishah2611@gmail.com>
2025-12-19 11:21:28 +0530

    final code live sever ip wise

--- frontend/src/utils/const.js
+++ frontend/src/utils/const.js
@@ -1,4 +1,5 @@
-const normalizedBase = ("http://localhost:5000/" ?? '').trim().replace(/\/$/, '');
+// const normalizedBase = ("http://localhost:5000/" ?? '').trim().replace(/\/$/, '');
+const normalizedBase = ("http://72.60.96.241:5000/" ?? '').trim().replace(/\/$/, '');
 
 export const API_BASE = normalizedBase;
 // Helper to build API URLs that works with or without a configured base
