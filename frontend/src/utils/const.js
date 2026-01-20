3e17c451fefd9395a8fac74569650c2ed251501e
khushalishah2611 <khushalishah2611@gmail.com>
2026-01-20 14:03:32 +0530

    final code

--- frontend/src/utils/const.js
+++ frontend/src/utils/const.js
@@ -1,6 +1,6 @@
-const normalizedBase = ("http://localhost:5000/" ?? '').trim().replace(/\/$/, '');
+// const normalizedBase = ("http://localhost:5000/" ?? '').trim().replace(/\/$/, '');
 // const normalizedBase = ("http://72.60.96.241:5000/" ?? '').trim().replace(/\/$/, '');
-// const normalizedBase = ("http://www.vedx.cloud:5000/" ?? '').trim().replace(/\/$/, '');
+const normalizedBase = ("http://www.vedx.cloud:5000/" ?? '').trim().replace(/\/$/, '');
 export const API_BASE = normalizedBase;
 // Helper to build API URLs that works with or without a configured base
 export const apiUrl = (path = '') => `${API_BASE}${path}`;
